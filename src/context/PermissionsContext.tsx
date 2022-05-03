b91b0726906be03ecfa81f39a87e78b11e22fbed
Jose Pardo <josmar.pm19@gmail.com>
2022-05-03 11:40:54 -0500

    project init

---
+++ src/context/PermissionsContext.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from "react";
+import { AppState, Platform } from "react-native";
+import { check, PERMISSIONS, PermissionStatus, request, openSettings } from "react-native-permissions";
+
+export interface PermissionsState{
+  locationStatus:PermissionStatus;
+}
+
+export const permissionsInitState:PermissionsState = {
+  locationStatus: 'unavailable'
+}
+
+type PermissionsContextProps = {
+  permissions:PermissionsState,
+  askLocationPermission:()=>void,
+  checkLocationPermission:()=>void
+}
+
+export const PermissionsContext = createContext({} as PermissionsContextProps);
+
+export const PermissionsProvider = ({children}:any) => {
+
+  const [permissions, setPermisssions] = useState(permissionsInitState)
+
+  useEffect(() => {
+    AppState.addEventListener('change', state => {
+      if(state !== 'active') return;
+      checkLocationPermission();
+    })
+  }, [])
+
+  const askLocationPermission = async() => {
+    let permissionsStatus:PermissionStatus;
+    if(Platform.OS === 'ios'){
+      permissionsStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+    }else{
+      permissionsStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+    }
+    if(permissionsStatus === 'blocked'){
+      openSettings();
+    }
+    setPermisssions({
+      ...permissions,
+      locationStatus: permissionsStatus
+    })
+  }
+
+  const checkLocationPermission = async() => {
+    let permissionsStatus:PermissionStatus;
+    if(Platform.OS === 'ios'){
+      permissionsStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+    }else{
+      permissionsStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+    }
+    setPermisssions({
+      ...permissions,
+      locationStatus: permissionsStatus
+    })
+  }
+
+  return(
+    <PermissionsContext.Provider value={{
+      permissions,
+      askLocationPermission,
+      checkLocationPermission
+
+    }}>
+      { children }
+    </PermissionsContext.Provider>
+  )
+
+}
---
+++ src/pages/PermissionsScreen.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { BlackButton } from '../components/BlackButton'
+import { PermissionsContext } from '../context/PermissionsContext'
+
+export const PermissionsScreen = () => {
+
+  const { permissions, askLocationPermission } = useContext(PermissionsContext)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Es NECESARIO el uso del GPS para usar esta aplicación</Text>
+      <BlackButton 
+        title="Permiso"
+        onPress={askLocationPermission}
+      />
+      <Text style={{marginTop: 20}}>
+        { JSON.stringify(permissions, null, 5) }
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title:{
+    width: 300,
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20
+  }
+});
